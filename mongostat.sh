2e5849b408bf816d0065c9ad0a563f4ff6779f1a
Ethan Steininger <ethan.steininger@mongodb.com>
2020-05-07 11:18:55 -0400

    no message

--- mongostat.sh
+++ mongostat.sh
@@ -1,5 +1,5 @@
-host=""
-username=""
-password=""
+host="sa-shared-demo-shard-00-01.lbvlu.mongodb.net:27017"
+username="ethan"
+password="1RrQIU5UZrp5Gci2"
 
 mongostat --host $host --ssl --username $username --password $password --authenticationDatabase admin | tee output.txt
